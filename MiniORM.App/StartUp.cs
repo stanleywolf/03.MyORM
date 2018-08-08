e94c1c036a83eda0289d9b5ba94867cd2a012c7b
stanleywolf <stanleyoffer@gmail.com>
2018-08-09 02:01:42 +0300

    test

---
+++ MiniORM.App/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using MiniORM.App.Data;
+using MiniORM.App.Data.Entities;
+
+namespace MiniORM.App
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = @"Server=.;Database=MiniORM;Integrated Security=True";
+
+            var context = new SoftUniDbContext(connectionString);
+
+            context.Employees.Add(new Employee
+            {
+               FirstName = "Mo",
+               LastName = "Salah",
+               DepartmentId = context.Departments.First().Id,
+               IsEmployed = true
+            });
+
+            //var employee = context.Employees.Last();
+            //employee.FirstName = "Zvone";
+            //employee.LastName = "Boban";
+
+            context.SaveChanges();
+        }
+    }
+}
